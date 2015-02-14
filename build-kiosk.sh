4d59c6fe608836ef65d82a6b6d89df78909f7579
probonopd <probono@puredarwin.org>
2015-02-14 16:15:48 +0100

    Update build-kiosk.sh

--- build-kiosk.sh
+++ build-kiosk.sh
@@ -4,18 +4,16 @@
 # https://github.com/raspberrypi/noobs/wiki/NOOBS-partitioning-explained
 # for the boot process. Root password is "raspberry" as we see in buildroot/.config
 
+#
+# This script needs root rights because otherwise the permissions inside
+# the recovery.rfs become messed up
+#
+
 wget http://downloads.raspberrypi.org/NOOBS_lite_latest -O NOOBS_lite_latest.zip
 unzip NOOBS_lite_latest.zip
 
 # "Install" lzop
-mkdir local
-cd local
-wget http://mirrors.kernel.org/ubuntu/pool/universe/l/lzop/lzop_1.02~rc1-2_amd64.deb
-ar x *.deb
-tar xfz data.tar.gz
-rm *.tar.gz *.deb debian-binary
-cd -
-export PATH=$PWD/local/usr/bin:$PATH
+apt-get -qq install lzop
 
 # Extract rootfs
 mkdir rootfs
